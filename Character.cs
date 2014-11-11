2738716bc5b582ff9b8696ff60d9b1cf084147ff
jojojangles <jcotosax@gmail.com>
2014-11-11 14:01:58 -0500

    Added more framework

--- Character.cs
+++ Character.cs
@@ -8,8 +8,14 @@ namespace PFCM
 {
     class Player
     {
-        List<Tuple<Pclass,int>> myClasses; //List of <class,level>
+        List<Tuple<Pclass, int>> myClasses; //List of <class object,level>
+        List<Tuple<Pskill, int>> mySkills; //List of <skill object,total>
+        Prace myRace; //Player race object
+        int STR,DEX,CON,INT,WIS,CHA; //Ability scores
+        Dictionary<Pbody,Pgear> myEquipped; //List of <body part object,gear object>
+        List<Tuple<Pgear, int>> myBackpack; //List of <gear object,qty>
 
+        
 
         static void Main(string[] args)
         {
